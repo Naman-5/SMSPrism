a2a8891f7a629ed34e656116981b2246454a8435
Nischith B <nischith.balaji@outlook.com>
2021-11-29 20:19:25 +0530

    Designed Welcome Page

---
+++ lib/res/onboarding_screen_items.dart
@@ -0,0 +1,19 @@
+List<Map> items = [
+  {
+    'header': 'SMS Prism',
+    'description': 'Fast and Simple Messaging App',
+    'image': 'assets/images/undraw_messages.svg'
+  },
+  {
+    'header': 'Smart Inbox',
+    'description':
+        'Automatically organizes messages so your inbox remains clutter-free',
+    'image': 'assets/images/undraw_mailbox.svg'
+  },
+  {
+    'header': 'Secure Messaging',
+    'description':
+        'Stay safe from Fraudulent and Malicious messages with inbuilt Spam detector',
+    'image': 'assets/images/undraw_updated.svg'
+  }
+];
